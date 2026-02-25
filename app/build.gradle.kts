7277bf09d6595374327c6d527d4ed25968be4649
YaerAy <yaer43670@gmail.com>
2026-02-25 17:14:31 +0800

    Temp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("kotlin-kapt")
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.example.course"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.course"
@@ -49,9 +47,9 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     // Room
-    val room_version = "2.6.1"
+    val room_version = "2.7.0-alpha11"
     implementation("androidx.room:room-runtime:$room_version")
-    kapt("androidx.room:room-compiler:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
 
     // Jsoup
